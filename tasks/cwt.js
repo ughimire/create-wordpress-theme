e7210826c6e127aa60084094a7f392671aaf5003
ughimire <me.umeshuser@gmail.com>
2021-02-08 22:17:19 +0545

    directry creation function added

--- tasks/cwt.js
+++ tasks/cwt.js
@@ -3,35 +3,56 @@
 'use strict';
 const fs = require('fs'); 
 const path = require('path'); 
- 
-console.log('Creating Theme Folder Name');  
-
-
-
 const readline = require("readline");
+
+const wpCreateThemeFolder = (theme_dir_name) => {
+    fs.mkdir(path.join(__dirname, theme_dir_name), (err) => { 
+            if (err) { 
+                return console.error(err); 
+            } 
+            console.log('Directory created successfully!'); 
+        }); 
+  };
+
+  const wpCreateThemeTemplates = (theme_name, theme_uri,author,author_uri) => {
+    var wp_theme_name = 'undefined',
+ wp_theme_uri='',
+ wp_author='',
+ wp_author_uri='',
+ wp_description='',
+ wp_text_domain='',
+ wp_tested_upto='5.5',
+ wp_requires_php='5.6',
+ wp_tags='';
+
+  };
+  
+  const getThemeName =(raw_name)=>{
+      var valid_name = raw_name.toLowerCase();
+      valid_name = valid_name.split(' ').join('-'); 
+      return valid_name;
+
+
+  };
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question("Theme Name: ", function(theme_name) {
+
+rl.question("Theme Name:", function(theme_name) {
     rl.question("Theme URI:", function(theme_uri) {
         rl.question("Author:", function(author) {
             rl.question("Author URI:", function(author_uri) {
-                console.log(`Theme Name: ${theme_name}`);
-                console.log(`Theme URI: ${theme_uri}`);
-                console.log(`Author: ${author}`);
-                console.log(`Author URI: ${author_uri}`);
+
+            var theme_valid_name = getThemeName(theme_name);
+               wpCreateThemeFolder(theme_valid_name);
+               //wpCreateThemeFiles()
                 rl.close();
             });
         });
     });
 });
 
+//console.log(wp_theme_name);
  return;
-fs.mkdir(path.join(__dirname, 'test'), (err) => { 
-    if (err) { 
-        return console.error(err); 
-    } 
-    console.log('Directory created successfully!'); 
-}); 
---
+++ tasks/tmpl/style.css.tpl
@@ -0,0 +1,16 @@
+/*
+Theme Name: {{theme_name}}
+Theme URI: {{theme_uri}}
+Author: {{author}}
+Author URI: {{author_uri}}
+Description: {{description}}
+Version: 1.0.0
+License: GNU General Public License v3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Text Domain: {{text_domain}}
+Tested up to: {{tested_upto}}
+Requires PHP: {{requires_php}}
+Tags: {{tags}}
+{{theme_name}} WordPress Theme, Copyright 2021 {{author}}
+{{theme_name}} is distributed under the terms of the GNU General Public License v3
+*/
