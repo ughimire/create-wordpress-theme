bcc0436b7cf015831bf93e527b843bcc1a8f5241
ughimire <me.umeshuser@gmail.com>
2021-02-08 21:51:27 +0545

    prompt added

--- tasks/cwt.js
+++ tasks/cwt.js
@@ -1,4 +1,37 @@
 #!/usr/bin/env node
 
 'use strict';
-console.log('This is Simple Application to check the global command');
+const fs = require('fs'); 
+const path = require('path'); 
+ 
+console.log('Creating Theme Folder Name');  
+
+
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Theme Name: ", function(theme_name) {
+    rl.question("Theme URI:", function(theme_uri) {
+        rl.question("Author:", function(author) {
+            rl.question("Author URI:", function(author_uri) {
+                console.log(`Theme Name: ${theme_name}`);
+                console.log(`Theme URI: ${theme_uri}`);
+                console.log(`Author: ${author}`);
+                console.log(`Author URI: ${author_uri}`);
+                rl.close();
+            });
+        });
+    });
+});
+
+ return;
+fs.mkdir(path.join(__dirname, 'test'), (err) => { 
+    if (err) { 
+        return console.error(err); 
+    } 
+    console.log('Directory created successfully!'); 
+}); 
